6e6e4e49bf936fdf528037db4eb72a4400808d17
Constantin Saguin <contact@wolfthemes.com>
2025-04-24 14:28:48 +0200

    fix: includes in init

--- wolf-playlist-manager.php
+++ wolf-playlist-manager.php
@@ -89,7 +89,7 @@ if ( ! class_exists( 'Wolf_Playlist_Manager' ) ) {
 			}
 
 			$this->define_constants();
-			$this->includes();
+			//$this->includes();
 			$this->init_hooks();
 
 			do_action( 'wpm_loaded' );
@@ -119,6 +119,7 @@ if ( ! class_exists( 'Wolf_Playlist_Manager' ) ) {
 		 */
 		private function init_hooks() {
 			add_action( 'after_setup_theme', array( $this, 'include_template_functions' ), 11 );
+			add_action( 'init', array( $this, 'includes' ), 0 );
 			add_action( 'init', array( $this, 'init' ), 0 );
 			register_activation_hook( __FILE__, array( $this, 'activate' ) );
 
